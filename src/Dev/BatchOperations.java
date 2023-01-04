f5a0d267b042b27a65a8b2966da7df8bd7c5d727
shubham1 <shubham1@xiaomi.com>
2023-01-04 11:26:38 +0530

    getBatches

---
+++ src/Dev/BatchOperations.java
@@ -0,0 +1,19 @@
+package Dev;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BatchOperations {
+    public static <T> List<List<T>> getBatches(List<T> list, int batchSize) {
+        int i = 0;
+        //logger.info(" total size : " + list.size() + " batch size : " + batchSize);
+        List<List<T>> batches = new ArrayList<List<T>>();
+        while (i < list.size()) {
+            int nextInc = Math.min(list.size() - i, batchSize);
+            List<T> batch = list.subList(i, i + nextInc);
+            batches.add(batch);
+            i = i + nextInc;
+        }
+        return batches;
+    }
+}
---
+++ src/stack/StackProblems.java
@@ -0,0 +1,11 @@
+package stack;
+
+public interface StackProblems {
+    /**
+     *
+     * @param exp - (a+b)+((c+d))
+     * @return true
+     */
+    boolean checkDuplicateBarcket(String exp);
+
+}
